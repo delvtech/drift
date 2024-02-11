f129bfba0e44cd823f8ec900c8785f0307bb43b4
Ryan Goree <goree.ryan@gmail.com>
2024-02-11 05:22:28 -0600

    Add viem package

---
+++ packages/evm-client-viem/src/index.ts
@@ -0,0 +1,25 @@
+// Errors
+export * from "@delvtech/evm-client/errors";
+
+// Contract
+export { createCachedReadContract } from "src/contract/createCachedReadContract";
+export { createCachedReadWriteContract } from "src/contract/createCachedReadWriteContract";
+export { createReadContract } from "src/contract/createReadContract";
+export { createReadWriteContract } from "src/contract/createReadWriteContract";
+
+// Network
+export { createNetwork } from "src/network/createViemNetwork";
+
+// Types
+export type { CreateCachedReadContractOptions } from "src/contract/createCachedReadContract";
+export type { CreateCachedReadWriteContractOptions } from "src/contract/createCachedReadWriteContract";
+export type {
+  CreateReadContractOptions,
+  ViemReadContract,
+} from "src/contract/createReadContract";
+export type { ReadWriteContractOptions } from "src/contract/createReadWriteContract";
+
+// Re-exports
+export { createLruSimpleCache } from "@delvtech/evm-client/factories";
+export * from "@delvtech/evm-client/types";
+export * from "@delvtech/evm-client/utils";
