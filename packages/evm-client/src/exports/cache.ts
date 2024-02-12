1ddc136e388b9fb11f90c0190fbb9271dfe4db74
Ryan Goree <goree.ryan@gmail.com>
2024-02-12 03:30:46 -0600

    Update exports

---
+++ packages/evm-client/src/exports/cache.ts
@@ -0,0 +1,3 @@
+export { createLruSimpleCache } from 'src/cache/factories/createLruSimpleCache';
+export type { SimpleCache, SimpleCacheKey } from 'src/cache/types/SimpleCache';
+export { createSimpleCacheKey } from 'src/cache/utils/createSimpleCacheKey';
