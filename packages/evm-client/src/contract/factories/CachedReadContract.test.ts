52edea93fe7ee35bdf9c0ce4099bdb584248f337
Danny Delott <dannydelott@gmail.com>
2024-04-29 12:46:54 -0700

    Add `deleteReadMatch` to CachedReadContract (#64)
    * Add isMatch from lodash
    
    * Convert undefined to null in createSimpleCacheKey, parse keys in cache.entries()
    
    * Add deleteReadMatch to CachedReadContract
    
    * Add changeset
    
    * Upgrade typescript version

--- packages/evm-client/src/contract/factories/CachedReadContract.test.ts
+++ packages/evm-client/src/contract/factories/CachedReadContract.test.ts
@@ -78,6 +78,76 @@ describe('createCachedReadContract', () => {
     expect(stub?.callCount).toBe(2);
   });
 
+  it('deletes cached reads from function name only', async () => {
+    const contract = new ReadContractStub(ERC20ABI);
+    const cachedContract = createCachedReadContract({ contract });
+
+    contract.stubRead({
+      functionName: 'balanceOf',
+      value: 100n,
+      args: { owner: ALICE },
+    });
+    contract.stubRead({
+      functionName: 'balanceOf',
+      value: 200n,
+      args: { owner: BOB },
+    });
+
+    // Get both alice and bob's balance
+    const aliceValue = await cachedContract.read('balanceOf', { owner: ALICE });
+    expect(aliceValue).toBe(100n);
+
+    const bobValue = await cachedContract.read('balanceOf', { owner: BOB });
+    expect(bobValue).toBe(200n);
+
+    // Deleting anything that matches a balanceOf call
+    cachedContract.deleteReadMatch('balanceOf');
+
+    // Request bob and alice's balance again
+    const aliceValue2 = await cachedContract.read('balanceOf', {
+      owner: ALICE,
+    });
+    expect(aliceValue2).toBe(100n);
+    const bobValue2 = await cachedContract.read('balanceOf', { owner: BOB });
+    expect(bobValue2).toBe(200n);
+
+    const stub = contract.getReadStub('balanceOf');
+    expect(stub?.callCount).toBe(4);
+  });
+
+  it('deletes cached reads with partial args', async () => {
+    const contract = new ReadContractStub(ERC20ABI);
+    const cachedContract = createCachedReadContract({ contract });
+
+    const aliceArgs = { owner: ALICE, spender: BOB } as const;
+    contract.stubRead({
+      functionName: 'allowance',
+      value: 100n,
+      args: aliceArgs,
+    });
+
+    const bobArgs = { owner: BOB, spender: ALICE } as const;
+    contract.stubRead({
+      functionName: 'allowance',
+      value: 200n,
+      args: bobArgs,
+    });
+
+    // Get both alice and bob's allowance
+    await cachedContract.read('allowance', aliceArgs);
+    await cachedContract.read('allowance', bobArgs);
+
+    // Deleting any allowance calls where BOB is the spender
+    cachedContract.deleteReadMatch('allowance', { spender: BOB });
+
+    // Request bob and alice's allowance again
+    await cachedContract.read('allowance', aliceArgs);
+    await cachedContract.read('allowance', bobArgs);
+
+    const stub = contract.getReadStub('allowance');
+    expect(stub?.callCount).toBe(3);
+  });
+
   it('clears the cache', async () => {
     const contract = new ReadContractStub(ERC20ABI);
     const cachedContract = createCachedReadContract({ contract });
