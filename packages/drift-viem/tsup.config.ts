654fddc52178fbc403671b50452f8bf3628540f4
Ryan Goree <goree.ryan@gmail.com>
2024-10-07 17:16:11 -0500

    Fix exports for cjs, update repo url

--- packages/drift-viem/tsup.config.ts
+++ packages/drift-viem/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts", "src/stubs.ts"],
-  format: ["esm"],
+  format: ["esm", "cjs"],
   sourcemap: true,
   dts: true,
   clean: true,
--- packages/drift/tsup.config.ts
+++ packages/drift/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/exports/index.ts", "src/exports/testing.ts"],
-  format: ["esm"],
+  format: ["esm", "cjs"],
   sourcemap: true,
   dts: true,
   clean: true,
