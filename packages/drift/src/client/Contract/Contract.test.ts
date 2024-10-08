d1b44bf631301e736bbed9e5fa5d9d026ecbf2ff
Ryan Goree <goree.ryan@gmail.com>
2024-10-08 05:06:51 -0500

    Add a few more tests

---
+++ packages/drift/src/client/Contract/Contract.test.ts
@@ -0,0 +1,51 @@
+import type { ContractEvent } from "src/adapter/types/Event";
+import { MockContract } from "src/client/contract/MockContract";
+import { erc20 } from "src/utils/testing/erc20";
+import { describe, expect, it } from "vitest";
+
+const abi = erc20.abi;
+type Erc20Abi = typeof abi;
+
+describe("Contract", () => {
+  it("Preloads events", async () => {
+    const contract = new MockContract({ abi });
+
+    const events: ContractEvent<Erc20Abi, "Transfer">[] = [
+      {
+        eventName: "Transfer",
+        args: {
+          from: "0xBob",
+          to: "0xAlice",
+          value: 100n,
+        },
+      },
+      {
+        eventName: "Transfer",
+        args: {
+          from: "0xAlice",
+          to: "0xBob",
+          value: 120n,
+        },
+      },
+    ];
+
+    contract.preloadEvents({ event: "Transfer", value: events });
+    expect(await contract.getEvents("Transfer")).toBe(events);
+  });
+
+  it("Preloads reads", async () => {
+    const contract = new MockContract({ abi });
+    contract.preloadRead({ fn: "symbol", value: "DAI" });
+    expect(await contract.read("symbol")).toBe("DAI");
+  });
+
+  it("Invalidates reads", async () => {
+    const contract = new MockContract({ abi });
+
+    contract.preloadRead({ fn: "symbol", value: "DAI" });
+    expect(await contract.read("symbol")).toBe("DAI");
+
+    contract.invalidateRead("symbol");
+    expect(contract.read("symbol")).rejects.toThrow();
+  });
+});
