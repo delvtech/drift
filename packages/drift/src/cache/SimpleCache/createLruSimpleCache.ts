de863930b7ce7382fbadddc56223abd0ec1bc7f4
Ryan Goree <goree.ryan@gmail.com>
2024-10-08 05:42:18 -0500

    Patch types

--- packages/drift/src/cache/SimpleCache/createLruSimpleCache.ts
+++ packages/drift/src/cache/SimpleCache/createLruSimpleCache.ts
@@ -9,23 +9,23 @@ import type { SerializableKey } from "src/utils/createSerializableKey";
  * [lru-cache](https://www.npmjs.com/package/lru-cache) library to provide LRU
  * caching capabilities conforming to the `SimpleCache` interface.
  *
- * @template TValue - The type of value to be stored in the cache.
- * @template TKey - The type of key used to access values in the cache.
+ * @template V - The type of value to be stored in the cache.
+ * @template K - The type of key used to access values in the cache.
  * @hidden
  */
 export function createLruSimpleCache<
-  TValue extends NonNullable<unknown> = NonNullable<unknown>,
-  TKey extends SerializableKey = SerializableKey,
->(options: LRUCache.Options<string, TValue, void>): SimpleCache<TValue, TKey> {
-  const cache = new LRUCache(options);
+  V extends NonNullable<unknown> = NonNullable<unknown>,
+  K extends SerializableKey = SerializableKey,
+>(options: LRUCache.Options<string, V, unknown>): SimpleCache<V, K> {
+  const cache = new LRUCache<string, V, unknown>(options);
 
   function* entriesGenerator(
-    originalGenerator: Generator<[TKey, TValue]>,
-  ): Generator<[TKey, TValue]> {
+    originalGenerator: Generator<[K, V]>,
+  ): Generator<[K, V]> {
     for (const [key, value] of originalGenerator) {
       // Modify the entry here before yielding it
       const modifiedEntry = [JSON.parse(key as string), value];
-      yield modifiedEntry as [TKey, TValue];
+      yield modifiedEntry as [K, V];
     }
   }
 
@@ -34,9 +34,9 @@ export function createLruSimpleCache<
       return cache.has(stringify(key));
     },
 
-    get entries() {
+    entries() {
       // Keys need to be returned in the same format as they were given to the cache
-      return entriesGenerator(cache.entries() as Generator<[TKey, TValue]>);
+      return entriesGenerator(cache.entries() as Generator<[K, V]>);
     },
 
     get(key) {
--- packages/drift/src/cache/SimpleCache/types.ts
+++ packages/drift/src/cache/SimpleCache/types.ts
@@ -4,40 +4,40 @@ import type { SerializableKey } from "src/utils/createSerializableKey";
  * Represents a simple caching mechanism with basic operations such as
  * get, set, delete, clear, and find.
  *
- * @template TValue - The type of value to be stored in the cache.
- * @template TKey - The type of key used to access values in the cache.
+ * @template V - The type of value to be stored in the cache.
+ * @template K - The type of key used to access values in the cache.
  * Must be a serializable value to ensure consistency and predictability.
  */
 export interface SimpleCache<
-  TValue = any,
-  TKey extends SerializableKey = SerializableKey,
+  V = any,
+  K extends SerializableKey = SerializableKey,
 > {
   /**
    * Returns an iterable of key-value pairs for every entry in the cache.
    */
-  readonly entries: Iterable<[TKey, TValue]>;
+  entries: () => Iterable<[K, V]>;
 
   /**
    * Returns a boolean indicating whether an entry exists for the specified key.
    */
-  has: (key: TKey) => boolean;
+  has: (key: K) => boolean;
 
   /**
    * Retrieves the value associated with the specified key.
    */
-  get: (key: TKey) => TValue | undefined;
+  get: (key: K) => V | undefined;
 
   /**
    * Associates the specified value with the specified key in the cache. If the
    * cache previously contained a mapping for the key, the old value is
    * replaced.
    */
-  set: (key: TKey, value: TValue) => void;
+  set: (key: K, value: V) => void;
 
   /**
    * Removes the mapping for the specified key from this cache if present.
    */
-  delete: (key: TKey) => void;
+  delete: (key: K) => void;
 
   /**
    * Removes all of the mappings from this cache.
@@ -50,7 +50,5 @@ export interface SimpleCache<
    *
    * @param predicate - A function to test each key-value pair in the cache.
    */
-  find: (
-    predicate: (value: TValue, key: TKey) => boolean,
-  ) => TValue | undefined;
+  find: (predicate: (value: V, key: K) => boolean) => V | undefined;
 }
