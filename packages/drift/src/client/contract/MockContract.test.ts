e3b8ce28a3d6ba7ae83343758cc80729dad99aea
Ryan Goree <goree.ryan@gmail.com>
2024-11-05 16:33:13 -0600

    Add `MockContract` tests for partial args

--- packages/drift/src/client/contract/MockContract.test.ts
+++ packages/drift/src/client/contract/MockContract.test.ts
@@ -8,6 +8,80 @@ const abi = erc20.abi;
 type Erc20Abi = typeof abi;
 
 describe("MockContract", () => {
+  describe("encodeFunctionData", () => {
+    it("Throws an error by default", async () => {
+      const contract = new MockContract({ abi });
+      let error: unknown;
+      try {
+        contract.encodeFunctionData("balanceOf", { account: "0x" });
+      } catch (e) {
+        error = e;
+      }
+      expect(error).toBeInstanceOf(Error);
+    });
+
+    it("Can be stubbed with specific args", async () => {
+      const contract = new MockContract({ abi });
+      contract
+        .onEncodeFunctionData("balanceOf", { account: "0x1" })
+        .returns("0x1");
+      contract
+        .onEncodeFunctionData("balanceOf", { account: "0x2" })
+        .returns("0x2");
+      expect(contract.encodeFunctionData("balanceOf", { account: "0x1" })).toBe(
+        "0x1",
+      );
+      expect(contract.encodeFunctionData("balanceOf", { account: "0x2" })).toBe(
+        "0x2",
+      );
+    });
+
+    it("Can be stubbed with partial args", async () => {
+      const contract = new MockContract({ abi });
+      contract.onEncodeFunctionData("balanceOf").returns("0x123");
+      expect(contract.encodeFunctionData("balanceOf", { account: "0x" })).toBe(
+        "0x123",
+      );
+    });
+
+    it("Can be called with args after being stubbed with no args", async () => {
+      const contract = new MockContract({ abi });
+      contract.onEncodeFunctionData().returns("0x123");
+      expect(contract.encodeFunctionData("balanceOf", { account: "0x" })).toBe(
+        "0x123",
+      );
+    });
+  });
+
+  describe("decodeFunctionData", () => {
+    it("Throws an error by default", async () => {
+      const contract = new MockContract({ abi });
+      let error: unknown;
+      try {
+        contract.decodeFunctionData("0x");
+      } catch (e) {
+        error = e;
+      }
+      expect(error).toBeInstanceOf(Error);
+    });
+
+    it("Can be stubbed with specific args", async () => {
+      const contract = new MockContract({ abi });
+      const decoded1: DecodedFunctionData<Erc20Abi, "balanceOf"> = {
+        functionName: "balanceOf",
+        args: { account: "0x1" },
+      };
+      const decoded2: DecodedFunctionData<Erc20Abi, "balanceOf"> = {
+        functionName: "balanceOf",
+        args: { account: "0x2" },
+      };
+      contract.onDecodeFunctionData("0x1").returns(decoded1);
+      contract.onDecodeFunctionData("0x2").returns(decoded2);
+      expect(contract.decodeFunctionData("0x1")).toBe(decoded1);
+      expect(contract.decodeFunctionData("0x2")).toBe(decoded2);
+    });
+  });
+
   describe("getEvents", async () => {
     it("Throws an error by default", async () => {
       const contract = new MockContract({ abi });
@@ -54,6 +128,24 @@ describe("MockContract", () => {
       expect(
         await contract.getEvents("Transfer", { filter: { from: "0x2" } }),
       ).toBe(events2);
+    });
+
+    it("Can be stubbed with partial args", async () => {
+      const contract = new MockContract({ abi });
+      const events: ContractEvent<Erc20Abi, "Transfer">[] = [
+        {
+          eventName: "Transfer",
+          args: {
+            from: "0x1",
+            to: "0x1",
+            value: 123n,
+          },
+        },
+      ];
+      contract.onGetEvents("Transfer").resolves(events);
+      expect(
+        await contract.getEvents("Transfer", { filter: { from: "0x1" } }),
+      ).toBe(events);
     });
 
     it("Inherits stubbed values from the adapter", async () => {
@@ -154,6 +246,14 @@ describe("MockContract", () => {
       ).toBe(false);
     });
 
+    it("Can be stubbed with partial args", async () => {
+      const contract = new MockContract({ abi });
+      contract.onSimulateWrite("transfer").resolves(true);
+      expect(
+        await contract.simulateWrite("transfer", { to: "0x1", amount: 123n }),
+      ).toBe(true);
+    });
+
     it("Inherits stubbed values from the adapter", async () => {
       const contract = new MockContract({ abi });
       contract.adapter
@@ -167,75 +267,6 @@ describe("MockContract", () => {
       expect(
         await contract.simulateWrite("transfer", { to: "0x", amount: 123n }),
       ).toBe(true);
-    });
-  });
-
-  describe("encodeFunctionData", () => {
-    it("Throws an error by default", async () => {
-      const contract = new MockContract({ abi });
-      let error: unknown;
-      try {
-        contract.encodeFunctionData("balanceOf", { account: "0x" });
-      } catch (e) {
-        error = e;
-      }
-      expect(error).toBeInstanceOf(Error);
-    });
-
-    it("Can be stubbed with specific args", async () => {
-      const contract = new MockContract({ abi });
-      contract
-        .onEncodeFunctionData("balanceOf", { account: "0x1" })
-        .returns("0x1");
-      contract
-        .onEncodeFunctionData("balanceOf", { account: "0x2" })
-        .returns("0x2");
-      expect(contract.encodeFunctionData("balanceOf", { account: "0x1" })).toBe(
-        "0x1",
-      );
-      expect(contract.encodeFunctionData("balanceOf", { account: "0x2" })).toBe(
-        "0x2",
-      );
-    });
-
-    it.todo(
-      "Can be called with args after being stubbed with no args",
-      async () => {
-        const contract = new MockContract({ abi });
-        contract.onEncodeFunctionData().returns("0x123");
-        expect(
-          contract.encodeFunctionData("balanceOf", { account: "0x" }),
-        ).toBe("0x123");
-      },
-    );
-  });
-
-  describe("decodeFunctionData", () => {
-    it("Throws an error by default", async () => {
-      const contract = new MockContract({ abi });
-      let error: unknown;
-      try {
-        contract.decodeFunctionData("0x");
-      } catch (e) {
-        error = e;
-      }
-      expect(error).toBeInstanceOf(Error);
-    });
-
-    it("Can be stubbed with specific args", async () => {
-      const contract = new MockContract({ abi });
-      const decoded1: DecodedFunctionData<Erc20Abi, "balanceOf"> = {
-        functionName: "balanceOf",
-        args: { account: "0x1" },
-      };
-      const decoded2: DecodedFunctionData<Erc20Abi, "balanceOf"> = {
-        functionName: "balanceOf",
-        args: { account: "0x2" },
-      };
-      contract.onDecodeFunctionData("0x1").returns(decoded1);
-      contract.onDecodeFunctionData("0x2").returns(decoded2);
-      expect(contract.decodeFunctionData("0x1")).toBe(decoded1);
-      expect(contract.decodeFunctionData("0x2")).toBe(decoded2);
     });
   });
 
@@ -282,15 +313,20 @@ describe("MockContract", () => {
       ).toBe("0x2");
     });
 
-    it.todo(
-      "Can be called with args after being stubbed with no arguments",
-      async () => {
-        const contract = new MockContract({ abi });
-        contract.onWrite().resolves("0x123");
-        expect(
-          await contract.write("transfer", { to: "0x", amount: 123n }),
-        ).toBe("0x123");
-      },
-    );
+    it("Can be stubbed with partial args", async () => {
+      const contract = new MockContract({ abi });
+      contract.onWrite("transfer").resolves("0x123");
+      expect(await contract.write("transfer", { to: "0x", amount: 123n })).toBe(
+        "0x123",
+      );
+    });
+
+    it("Can be called with args after being stubbed with no arguments", async () => {
+      const contract = new MockContract({ abi });
+      contract.onWrite().resolves("0x123");
+      expect(await contract.write("transfer", { to: "0x", amount: 123n })).toBe(
+        "0x123",
+      );
+    });
   });
 });
