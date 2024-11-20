7ce8e6dd04a22817a8dcb5ef1e6a37324112d717
Ryan Goree <goree.ryan@gmail.com>
2024-11-20 14:19:40 -0600

    Remove comment

--- packages/drift/src/adapter/types/Block.ts
+++ packages/drift/src/adapter/types/Block.ts
@@ -1,8 +1,6 @@
 import type { Address, Bytes, Hash } from "src/adapter/types/Abi";
 
 // https://github.com/ethereum/execution-apis/blob/3ae3d29fc9900e5c48924c238dff7643fdc3680e/src/schemas/block.yaml#L1
-// export type Block = PendingBlock | MinedBlock;
-
 export interface BaseBlockProps {
   extraData?: Bytes;
   gasLimit: bigint;
