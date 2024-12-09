e7d268631a5917deb8e8b8dd573aea8e246f7329
Ryan Goree <goree.ryan@gmail.com>
2024-12-08 23:53:30 -0600

    Add `CodeCaller` contract

---
+++ contracts/.prettierrc.cjs
@@ -0,0 +1,5 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  tabWidth: 4,
+  useTabs: false,
+};
---
+++ contracts/src/CodeCaller.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.24;
+
+/**
+ * @title CodeCaller
+ * @notice A contract that creates a contract and calls it
+ */
+contract CodeCaller {
+    error BytecodeDeploymentFailed();
+
+    constructor(bytes memory bytecode, bytes memory callParams) {
+        // Deploy the contract with the CREATE opcode
+        address deployed;
+        assembly {
+            deployed := create(0, add(bytecode, 32), mload(bytecode))
+        }
+
+        // Revert if the deployment failed
+        if (deployed == address(0) || deployed.code.length == 0) {
+            revert BytecodeDeploymentFailed();
+        }
+
+        // Call the deployed contract
+        (bool success, bytes memory returnData) = deployed.call(callParams);
+
+        // Forward the return data
+        assembly {
+            if iszero(success) {
+                revert(add(returnData, 32), mload(returnData))
+            }
+            return(add(returnData, 32), mload(returnData))
+        }
+    }
+}
