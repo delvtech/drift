f8b1d59e4460cd06cd8b8f96ec610a2dfd2438d4
Ryan Goree <goree.ryan@gmail.com>
2025-03-01 18:19:13 -0600

    Simplify `LruStore`

--- packages/drift/src/store/LruStore.ts
+++ packages/drift/src/store/LruStore.ts
@@ -1,5 +1,4 @@
 import { LRUCache } from "lru-cache";
-import stringify from "safe-stable-stringify";
 import type { CacheStore } from "src/store/types";
 
 export type LruStoreConfig<
@@ -14,40 +13,11 @@ export type LruStoreConfig<
  *
  * @see [NPM - lru-cache](https://www.npmjs.com/package/lru-cache).
  */
-export class LruStore implements CacheStore {
-  private _lru: LRUCache<string, any, unknown>;
-
-  constructor(config: LruStoreConfig<any> = { max: 500 }) {
-    this._lru = new LRUCache(config);
-  }
-
-  *entries() {
-    for (const [key, value] of this._lru.entries()) {
-      yield [JSON.parse(key), value] as [string, any];
-    }
-  }
-
-  find(predicate: (value: any, key: string) => boolean) {
-    return this._lru.find((value, key) => predicate(value, JSON.parse(key)));
-  }
-
-  has(key: string) {
-    return this._lru.has(stringify(key));
-  }
-
-  get(key: string) {
-    return this._lru.get(stringify(key));
-  }
-
-  set(key: string, value: any) {
-    this._lru.set(stringify(key), value);
-  }
-
-  delete(key: string) {
-    this._lru.delete(stringify(key));
-  }
-
-  clear() {
-    this._lru.clear();
+export class LruStore
+  extends LRUCache<string, any, unknown>
+  implements CacheStore
+{
+  constructor(options: LruStoreConfig = { max: 500 }) {
+    super(options);
   }
 }
