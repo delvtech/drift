17209386cd160f8dc80a4c38ab072c55b1075d35
Ryan Goree <goree.ryan@gmail.com>
2025-03-17 12:21:12 -0500

    Simplify util

--- packages/drift/src/utils/stringifyKey.ts
+++ packages/drift/src/utils/stringifyKey.ts
@@ -1,12 +1,9 @@
 import stringify from "safe-stable-stringify";
 
 export function stringifyKey<T>(rawKey: T): Stringified<T> {
-  return stringify(rawKey, (_, v) => {
-    if (typeof v === "bigint") {
-      return v.toString();
-    }
-    return v;
-  }) as Stringified<T>;
+  return stringify(rawKey, (_, v) =>
+    typeof v === "bigint" ? v.toString() : v,
+  ) as Stringified<T>;
 }
 
 type Stringified<T> = string | Extract<T, undefined>;
