5a16e3670e76952c99246997ef9120e22caf6e64
Ryan Goree <goree.ryan@gmail.com>
2025-06-09 13:28:29 -0500

    Combine lib import contracts

--- contracts/src/Lib.sol
+++ contracts/src/Lib.sol
@@ -4,5 +4,6 @@ pragma solidity ^0.8.24;
 // Ensures these interfaces are included in the build artifacts
 import {IERC20} from "forge-std/interfaces/IERC20.sol";
 import {IERC721} from "forge-std/interfaces/IERC721.sol";
-import {IERC4626} from "forge-std/interfaces/IERC4626.sol";
 import {IERC1155} from "forge-std/interfaces/IERC1155.sol";
+import {IERC4626} from "forge-std/interfaces/IERC4626.sol";
+import {IMulticall3} from "forge-std/interfaces/IMulticall3.sol";
