6772e43bb4b1e1dc8808b9e5012f7d5588581753
Ryan Goree <goree.ryan@gmail.com>
2025-07-03 16:18:26 -0500

    Refactor bytecode call example to include constructor args

--- examples/node.js/src/bytecode_call.ts
+++ examples/node.js/src/bytecode_call.ts
@@ -1,5 +1,10 @@
-import { createDrift } from "@delvtech/drift";
-import { MockErc20Example } from "src/abis/MockErc20Example";
+import {
+  createDrift,
+  decodeFunctionReturn,
+  encodeDeployData,
+  encodeFunctionData,
+} from "@delvtech/drift";
+import { testToken } from "@delvtech/drift/testing";
 
 const rpcUrl = process.env.RPC_URL;
 const drift = createDrift({ rpcUrl });
@@ -7,21 +12,33 @@ const drift = createDrift({ rpcUrl });
 // An example of a bytecode, i.e., "deployless", call in which the bytecode of a
 // contract is temporarily created to call a function on it.
 
-const data = await drift.call({
-  bytecode: MockErc20Example.bytecode,
-  data: drift.encodeFunctionData({
-    abi: MockErc20Example.abi,
-    fn: "name",
-  }),
+const deployData = encodeDeployData({
+  abi: testToken.abi,
+  bytecode: testToken.bytecode,
+  args: {
+    decimals_: 18,
+    initialSupply: 1_500_000n * BigInt(1e18),
+  },
 });
 
-console.log("response data:", data);
+const callData = encodeFunctionData({
+  abi: testToken.abi,
+  fn: "totalSupply",
+});
 
-if (data) {
-  const decoded = drift.decodeFunctionReturn({
-    abi: MockErc20Example.abi,
-    fn: "name",
-    data,
+const returnData = await drift.call({
+  bytecode: deployData,
+  data: callData,
+});
+
+console.log("response data:", returnData);
+
+if (returnData) {
+  const decoded = decodeFunctionReturn({
+    abi: testToken.abi,
+    fn: "totalSupply",
+    data: returnData,
   });
+
   console.log("decoded:", decoded);
 }
